97793e46138221e89c05030ff6828f8eeb566293
Ramon Ryon <ramonryon@gmail.com>
2023-11-06 17:02:00 -0300

    add playlist e main section album

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,80 @@
-import { Home as HomeIcon, Search, Library } from "lucide-react"
+import { Home as HomeIcon, Search, Library, ChevronLeft, ChevronRight } from "lucide-react"
+import Image from "next/image"
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col">
       <div className="flex flex-1">
         <aside className="w-72 bg-zinc-950 p-6">
-          <nav className="space-y-4">
-            <a href="" className="flex items-center gap-2 text-xs font-semibold text-zinc-100">
+          <nav className="space-y-5">
+            <a href="" className="flex items-center gap-2 text-sm font-semibold text-zinc-200">
               <HomeIcon />
               Home
             </a>
-            <a href="" className="flex items-center gap-2 text-xs font-semibold text-zinc-100">
+            <a href="" className="flex items-center gap-2 text-sm font-semibold text-zinc-200">
               <Search />
               Search
             </a>
-            <a href="" className="flex items-center gap-2 text-xs font-semibold text-zinc-100">
+            <a href="" className="flex items-center gap-2 text-sm font-semibold text-zinc-200">
               < Library />
               Your Library
             </a>
           </nav>
+
+          <nav className="mt-6 pt-6 border-t border-zinc-800 flex flex-col gap-3">
+            <a href="" className=" text-sm text-zinc-400 hover:text-zinc-100">Cardio Crush</a>
+            <a href="" className=" text-sm text-zinc-400 hover:text-zinc-100">Workout Warriors</a>
+            <a href="" className=" text-sm text-zinc-400 hover:text-zinc-100">Gym Groove</a>
+            <a href="" className=" text-sm text-zinc-400 hover:text-zinc-100">Run Rhythms</a>
+            <a href="" className=" text-sm text-zinc-400 hover:text-zinc-100">Pump It Up!</a>
+            <a href="" className=" text-sm text-zinc-400 hover:text-zinc-100">Fitness Frenzy</a>
+            <a href="" className=" text-sm text-zinc-400 hover:text-zinc-100">Sweat Session Sounds</a>
+            <a href="" className=" text-sm text-zinc-400 hover:text-zinc-100">Crossfit Jamz</a>
+            <a href="" className=" text-sm text-zinc-400 hover:text-zinc-100">Yoga Vibes</a>
+            <a href="" className=" text-sm text-zinc-400 hover:text-zinc-100">Chill Cool-Downs</a>
+
+          </nav>
         </aside>
-        <main className="flex-1 p-6">main</main>
+        <main className="flex-1 p-6">
+          <div className="flex items-center gap-4">
+            <button className="rounded-full bg-black/40 p-2">
+              <ChevronLeft />
+            </button>
+            <button className="rounded-full bg-black/40 p-2">
+              <ChevronRight />
+            </button>
+          </div>
+
+          <h1 className="font-semibold text-3xl mt-10">Good Afternoon</h1>
+
+
+          <div className="grid grid-cols-3 gap-4 mt-4">
+            <div className="bg-white/10 rounded flex items-center gap-4 overflow-hidden">
+              <Image src="/album.webp" width={104} height={104} alt="Capa do album somewhere in time da banda Iron maiden" />
+              <strong>Somewhere In Time</strong>
+            </div>
+            <div className="bg-white/10 rounded flex items-center gap-4 overflow-hidden">
+              <Image src="/album.webp" width={104} height={104} alt="Capa do album somewhere in time da banda Iron maiden" />
+              <strong>Somewhere In Time</strong>
+            </div>
+            <div className="bg-white/10 rounded flex items-center gap-4 overflow-hidden">
+              <Image src="/album.webp" width={104} height={104} alt="Capa do album somewhere in time da banda Iron maiden" />
+              <strong>Somewhere In Time</strong>
+            </div>
+            <div className="bg-white/10 rounded flex items-center gap-4 overflow-hidden">
+              <Image src="/album.webp" width={104} height={104} alt="Capa do album somewhere in time da banda Iron maiden" />
+              <strong>Somewhere In Time</strong>
+            </div>
+            <div className="bg-white/10 rounded flex items-center gap-4 overflow-hidden">
+              <Image src="/album.webp" width={104} height={104} alt="Capa do album somewhere in time da banda Iron maiden" />
+              <strong>Somewhere In Time</strong>
+            </div>
+            <div className="bg-white/10 rounded flex items-center gap-4 overflow-hidden">
+              <Image src="/album.webp" width={104} height={104} alt="Capa do album somewhere in time da banda Iron maiden" />
+              <strong>Somewhere In Time</strong>
+            </div>
+          </div>
+        </main>
       </div>
       <footer className="bg-zinc-800 border-t border-zinc-700 p-6">footer</footer>
     </div>
